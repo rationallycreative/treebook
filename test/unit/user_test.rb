089164c0551bc6a07fa6f07775e84eca5d2b8d17
Mike Lorey <mdlorey@gmail.com>
2013-09-20 14:33:13 -0400

    Added tests for user profile correctness.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -18,4 +18,21 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 	end
+
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.profile_name = users(:jason).profile_name
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new
+		user.profile_name = "My Profile With Spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("must be formatted correctly.")
+	end
 end
